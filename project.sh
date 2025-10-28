96f5fb68ebcc0a74d08b8daef8812ad74bf3a407
JeffreyYau <jy2870@rit.edu>
2025-10-28 16:34:01 -0400

    Starting bash

---
+++ project.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROC_NAME="$1"
+INTERVAL=5
+OUTFILE="${PROC_NAME}_metrics.csv"
+
+PS_OUTPUT= ps aux | egrep "$PROC_NAME"
+echo $PS_OUTPUT
+
+#while true; do
+	# Get metrics for all matching processes (excluding the grep itself)
+#	PS_OUTPUT= ps aux | egrep "$PROC_NAME"
+#	echo $PS_OUTPUT
+
+#done
+
+
+
+
